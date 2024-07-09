aeaa6cab306fa7250ef5c6b666ce6bc5d39ea9e8
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-09 13:57:49 +0800

    feat: add resource route for SuratMasuk and SuratKeluar.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SuratMasukController;
+use App\Http\Controllers\SuratKeluarController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +24,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Route for SuratMasuk and SuratKeluar
+    Route::resource('surat_masuk', SuratMasukController::class);
+    Route::resource('surat_keluar', SuratKeluarController::class);
 });
 
 require __DIR__ . '/auth.php';
