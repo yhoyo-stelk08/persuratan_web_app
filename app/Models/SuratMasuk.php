53c71dc2d59d70ac89cc125e346bb312c29c6d8f
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-09 21:25:39 +0800

    feat: create the fillable properties

--- app/Models/SuratMasuk.php
+++ app/Models/SuratMasuk.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class SuratMasuk extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'tanggal_naskah',
+        'nomor_naskah',
+        'hal',
+        'asal_naskah',
+        'sifat_arsip',
+        'kode_klasifikasi',
+        'kode_unit',
+        'uraian_info_berkas',
+        'tingkat_perkembangan',
+        'jumlah_halaman_surat',
+        'lokasi',
+        'masa_aktif',
+        'masa_inaktif',
+        'keterangan',
+        'jumlah_folder',
+    ];
 }
