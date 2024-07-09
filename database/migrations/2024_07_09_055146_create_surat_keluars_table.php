12d38c9fec40e4fc0447a9517632a78aea1364b9
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-09 19:10:58 +0800

    feat: modify the migrations files for SuratMasuk n SuratKeluar.

--- database/migrations/2024_07_09_055146_create_surat_keluars_table.php
+++ database/migrations/2024_07_09_055146_create_surat_keluars_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,21 @@ return new class extends Migration
     {
         Schema::create('surat_keluars', function (Blueprint $table) {
             $table->id();
+            $table->date('tanggal_naskah');
+            $table->string('nomor_naskah', 255);
+            $table->text('hal');
+            $table->text('asal_naskah');
+            $table->enum('sifat_arsip', ['BIASA', 'TERBATAS', 'RAHASIA']);
+            $table->string('kode_klasifikasi', 255);
+            $table->string('kode_unit', 255)->nullable();
+            $table->text('uraian_info_berkas')->nullable();
+            $table->enum('tingkat_perkembangan', ['Copy', 'Asli']);
+            $table->integer('jumlah_halaman_surat')->nullable()->default(0);
+            $table->string('lokasi', 255)->nullable();
+            $table->integer('masa_aktif')->default(1);
+            $table->integer('masa_inaktif')->default(1);
+            $table->enum('keterangan', ['Musnah', 'Permanen']);
+            $table->integer('jumlah_folder')->nullable();
             $table->timestamps();
         });
     }
