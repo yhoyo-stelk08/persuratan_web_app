5e2a45ce7791c5cc2f3e02053e9d858534129d05
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-09 17:50:51 +0800

    feat: create pages for the front end.

--- resources/js/Pages/SuratKeluar/Index.jsx
+++ resources/js/Pages/SuratKeluar/Index.jsx
@@ -1,24 +1,22 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 
-export default function Dashboard({ auth }) {
+export default function SuratKeluarIndex({ auth }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
             header={
                 <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    Home
+                    Surat Keluar
                 </h2>
             }
         >
-            <Head title="Home" />
+            <Head title="Surat Keluar" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            You're logged in!
-                        </div>
+                        <div className="p-6 text-gray-900">Surat Keluar</div>
                     </div>
                 </div>
             </div>
--- resources/js/Pages/SuratMasuk/Index.jsx
+++ resources/js/Pages/SuratMasuk/Index.jsx
@@ -1,24 +1,22 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 
-export default function Dashboard({ auth }) {
+export default function SuratMasukIndex({ auth }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
             header={
                 <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    Home
+                    Surat Masuk
                 </h2>
             }
         >
-            <Head title="Home" />
+            <Head title="Surat Masuk" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            You're logged in!
-                        </div>
+                        <div className="p-6 text-gray-900">Surat Masuk</div>
                     </div>
                 </div>
             </div>
