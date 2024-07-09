7ac8867e002f17a9fc8c3becd35f70c021cb8a91
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-09 13:54:35 +0800

    feat: create controller for SuratMasuk & SuratKeluar.

---
+++ app/Http/Controllers/SuratMasukController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SuratMasuk;
+use Illuminate\Http\Request;
+
+class SuratMasukController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SuratMasuk $suratMasuk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SuratMasuk $suratMasuk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SuratMasuk $suratMasuk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SuratMasuk $suratMasuk)
+    {
+        //
+    }
+}
