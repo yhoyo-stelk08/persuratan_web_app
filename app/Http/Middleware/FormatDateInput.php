8505a15cede8ece829039daf6a7fa987b68594fd
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-10 21:57:45 +0800

    feat: formatting date to format yyyy-mm-dd.

--- app/Http/Middleware/FormatDateInput.php
+++ app/Http/Middleware/FormatDateInput.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Carbon\Carbon;
 
 class FormatDateInput
 {
@@ -15,6 +16,18 @@ class FormatDateInput
      */
     public function handle(Request $request, Closure $next): Response
     {
+        $dateFields = ['tanggal_naskah']; // Add all the date fields that need formatting here
+
+        foreach ($dateFields as $field) {
+            if ($request->has($field)) {
+                try {
+                    $formattedDate = Carbon::createFromFormat('F j, Y', $request->input($field))->format('Y-m-d');
+                    $request->merge([$field => $formattedDate]);
+                } catch (\Exception $e) {
+                    return response()->json(['error' => 'Invalid date format for field ' . $field], 422);
+                }
+            }
+        }
         return $next($request);
     }
 }
