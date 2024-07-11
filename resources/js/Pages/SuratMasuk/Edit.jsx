0a0c60b85b983b9eb61cb450a8cdcbb805091b69
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-11 21:43:38 +0800

    feat: create new page EditSuratMasuk page.

---
+++ resources/js/Pages/SuratMasuk/Edit.jsx
@@ -0,0 +1,31 @@
+import FormInputUpdateSurat from "@/Components/FormInputUpdateSurat";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+
+export default function EditSuratMasuk({ auth, surat_masuk }) {
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Update Surat Masuk
+                </h2>
+            }
+        >
+            <Head title="Update Surat Masuk" />
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
+                            <FormInputUpdateSurat
+                                submitRoute={"surat-masuk.update"}
+                                surat_masuk={surat_masuk}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
