d5af3c1e3faa27f28f6fc91f9fc09ac62a62af99
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-11 12:05:33 +0800

    feat: create SuratMasuk & SuratKeluar resource, and use the FormatSurat trait in both.

---
+++ app/Http/Resources/SuratKeluarResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Traits\FormatSurat;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SuratKeluarResource extends JsonResource
+{
+    use FormatSurat;
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return $this->formatSuratArray($this);
+    }
+}
