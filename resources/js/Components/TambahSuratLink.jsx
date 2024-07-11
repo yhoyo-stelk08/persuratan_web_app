ba9bea3a18a17772dc8f80236229a493f2ff4394
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-11 12:59:12 +0800

    feat: modify the css styles.

--- resources/js/Components/TambahSuratLink.jsx
+++ resources/js/Components/TambahSuratLink.jsx
@@ -2,7 +2,7 @@ import { Link } from "@inertiajs/react";
 
 const TambahSuratLink = ({ routeName, className, children }) => {
     return (
-        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+        <div className="mt-4 flex justify-end items-center sm:mt-0 sm:ml-16 sm:flex-none">
             <Link
                 href={route(routeName)}
                 className={`inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto ${className}`}
