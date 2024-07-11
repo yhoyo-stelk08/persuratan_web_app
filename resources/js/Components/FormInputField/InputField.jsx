3c92bf088bfb8cf487ddc4ca3082ecc261702a1d
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-11 14:06:36 +0800

    feat: create new component InputField.

---
+++ resources/js/Components/FormInputField/InputField.jsx
@@ -0,0 +1,57 @@
+import { Label, TextInput } from "flowbite-react";
+
+const inputType = {
+    InputText: "InputText",
+    InputTextArea: "InputTextArea",
+    InputSelect: "InputSelect",
+    InputDatePicker: "InputDate",
+    InputNumber: "InputNumber",
+};
+export default function FormInputTextInput({
+    id,
+    name,
+    label,
+    errorMsg,
+    formInputType,
+    ...props
+}) {
+    return (
+        <>
+            <Label
+                htmlFor={id}
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+                {label}
+            </Label>
+            {formInputType == inputType.InputText && (
+                <TextInput
+                    name={name}
+                    id={id}
+                    placeholder={label}
+                    type="text"
+                    {...props}
+                />
+            )}
+
+            {formInputType == inputType.InputNumber && (
+                <TextInput
+                    name={name}
+                    id={id}
+                    placeholder={label}
+                    type="number"
+                    {...props}
+                />
+            )}
+
+            {/* {
+                inputType.InputTextArea && (
+
+                )
+            } */}
+
+            {errorMsg ? (
+                <p className="text-sm text-red-600 ">{errorMsg}</p>
+            ) : null}
+        </>
+    );
+}
