39d7b334d1b1ba5d0463ee26c8ff380e7bb1ca30
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-12 00:03:07 +0800

    feat: create new page ShowSuratMasuk.

---
+++ resources/js/Pages/SuratMasuk/Show.jsx
@@ -0,0 +1,28 @@
+import FormShowSurat from "@/Components/FormShowSurat";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+
+export default function ShowSuratMasuk({ auth, surat_masuk }) {
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Detail Surat Masuk
+                </h2>
+            }
+        >
+            <Head title="Detail Surat Masuk" />
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
+                            <FormShowSurat surat_masuk={surat_masuk} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
